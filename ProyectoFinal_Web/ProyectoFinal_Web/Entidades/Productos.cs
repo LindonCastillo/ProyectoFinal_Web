f1a18f89c400a42148268452969b285c9c8bbf47
Lindon Emil Castillo Burgos <lindon_castillo@ucne.edu.do>
2020-01-26 22:45:16 -0400

    Agregando la entidad Productos

---
+++ ProyectoFinal_Web/ProyectoFinal_Web/Entidades/Productos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_Web.Entidades
+{
+    public class Productos
+    {
+        [Key]   
+        public int ProductoId { get; set; }
+        public string Nombre { get; set; }
+        public DateTime Fecha { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioCompra { get; set; }
+        public decimal PrecioVenta { get; set; }
+        public int Itbis { get; set; }
+
+        public Productos()
+        {
+            ProductoId = 0;
+            Nombre = string.Empty;
+            Fecha = DateTime.Now;
+            Cantidad = 0;
+            PrecioCompra = 0;
+            PrecioVenta = 0;
+            Itbis = 0;
+        }
+    }
+}
