1829cfb35193f28c8e5f4473b5445a30fad6a373
Lindon Emil Castillo Burgos <lindon_castillo@ucne.edu.do>
2020-01-26 22:54:55 -0400

    Agrengando la Clase Contexto

---
+++ ProyectoFinal_Web/ProyectoFinal_Web/Data/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinal_Web.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_Web.Data
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Productos> Productos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server = .\SqlExpress; Database= PFTest_Db; Trusted_Connection = True;");
+        }
+    }
+}
