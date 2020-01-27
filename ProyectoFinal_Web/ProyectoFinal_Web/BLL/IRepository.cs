1062a80570923c79b56c409a95b0d922b74aa44e
Lindon Emil Castillo Burgos <lindon_castillo@ucne.edu.do>
2020-01-26 23:18:14 -0400

    Agregando El Patron Repositorio

---
+++ ProyectoFinal_Web/ProyectoFinal_Web/BLL/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_Web.BLL
+{
+    public interface IRepository <T> where T : class
+    {
+        List<T> GetList(Expression<Func<T, bool>> Expression);
+        T Buscar(int Id);
+        bool Guardar(T entity);
+        bool Modificar(T entity);
+        bool Eliminar(int Id);
+    }
+}
---
+++ ProyectoFinal_Web/ProyectoFinal_Web/BLL/RepositorioBase.cs
@@ -0,0 +1,114 @@
+﻿using ProyectoFinal_Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProyectoFinal_Web.BLL;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProyectoFinal_Web.BLL
+{
+    public class RepositorioBase<T> : IDisposable, IRepository<T> where T : class
+    {
+        internal Contexto contexto;
+
+        public RepositorioBase()
+        {
+            contexto = new Contexto();
+        }
+
+        public virtual bool Guardar(T entity)
+        {
+            bool paso = false;
+
+            try
+            {
+                if (contexto.Set<T>().Add(entity) != null)
+                    paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return paso;
+        }
+        public virtual bool Modificar(T entity)
+        {
+            bool paso = false;
+
+            try
+            {
+
+                contexto.Entry(entity).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return paso;
+        }
+        public T Buscar(int id)
+        {
+            T entity;
+
+            try
+            {
+                entity = contexto.Set<T>().Find(id);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return entity;
+        }
+        public List<T> GetList(Expression<Func<T, bool>> expression)
+        {
+            List<T> lista;
+
+            try
+            {
+                lista = contexto.Set<T>().Where(expression).ToList();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return lista;
+        }
+        public bool Eliminar(int id)
+        {
+            bool paso = false;
+            T entity;
+
+            try
+            {
+                entity = contexto.Set<T>().Find(id);
+                contexto.Entry(entity).State = EntityState.Deleted;
+                paso = contexto.SaveChanges() > 0;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return paso;
+        }
+
+
+        public void Dispose()
+        {
+            contexto.Dispose();
+        }
+    }
+}
